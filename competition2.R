d86047b56c1f99b16f77a9c3af7d342207389641
DESKTOP-2LPI0J9\Camilo <aguilarcamiloa@gmail.com>
2018-04-12 23:16:16 -0500

    best model
    Algoritmo para selección del mejor modelo

--- competition2.R
+++ competition2.R
@@ -6,107 +6,109 @@ options(survey.lonely.psu = "adjust")
 options(scipen=999)
 rm(list = ls())
 
-## NI: Numero de municipios en la pob,  
-## NII: Número de colegios, 
-## N_i: Número estudiantes
+## parámetros de interés:
+##  * Promedio del ingreso 
+##  * Proporción de desempleados por municipio.
+## POBLACION: Numero de personas por municipio año 2014.  
 
-##setwd("C:/Users/Camilo/Documents/GitHub/SAE_competition2")
+## setwd("C:/Users/Camilo/Documents/GitHub/SAE_competition2")
 
-muestra <- readRDS("./data/muestra3etapasACundinamarca.RDS")
-length(unique(muestra$CODIGOMUNICIPIO))
-InfoAux <- as.data.frame(read_excel("./data/CONSOLIDADO_MPIOS_CUNDINAMARCA.xlsx"))
+muestra_multi <- readRDS("./data/df.RDS")
+muestra_multi <- muestra_multi[,c(1,10,9,2:8,11:length(names(muestra_multi)))]
+muestra_multi <- muestra_multi %>% select(-(POBLACION2014))  
+muestra_multi$densidad_pop <- muestra_multi$POBLACION / muestra_multi$AREA_KM2
+muestra_multi$Biblio_per <- muestra_multi$POBLACION / muestra_multi$NUM_BIBLIOTECAS
+muestra_multi$Biblio_per[is.infinite(muestra_multi$Biblio_per)]<-0
+muestra_multi$Tasa_secuestros <- muestra_multi$PERSONAS_SECUESTRADAS / muestra_multi$POBLACION
+muestra_multi$Tasa_recibidos <- muestra_multi$PERSONAS_RECIBIDOS / muestra_multi$POBLACION
 
-# Mejor traerle los nombres  y códigos de los municipios DANE (los códigos mpio ICFES cambian)
-# Ver si se puede unir por los nombres de los municipios 
-mun <- InfoAux$MCPIO 
-mun2 <- unique(muestra$NOMBREMUNICIPIO)
-table(mun2%in% mun)
+# Datos poblacionales
+Municipios <- as.data.frame(read_excel("./data/CONSOLIDADO_MPIOS_CUNDINAMARCA.xlsx"))
+Municipios$densidad_pop <- Municipios$POBLACION / Municipios$AREA_KM2
+Municipios$Biblio_per <- Municipios$POBLACION / Municipios$NUM_BIBLIOTECAS
+Municipios$Biblio_per[is.infinite(Municipios$Biblio_per)]<-0
+Municipios$Tasa_secuestros <- Municipios$PERSONAS_SECUESTRADAS / Municipios$POBLACION
+Municipios$Tasa_recibidos <- Municipios$PERSONAS_RECIBIDOS / Municipios$POBLACION
 
-Nomb_mpios <- InfoAux %>% group_by(ID_DANE, MCPIO) %>% summarise (temp = n())
-Nomb_mpios$temp <- NULL
-muestra <- merge(Nomb_mpios, muestra, all.y = T,
-                 by.x = "MCPIO", by.y = "NOMBREMUNICIPIO")
+names(muestra_multi)
 
-### Paso 1: primero calcular las estimaciones directas de los municipios
-diseno_muestral <- svydesign(ids = ~ID_DANE + CODIGO_ICFES + ID_estud,
-                             strata = ~estrato_mpio + EstratoColegio,
-                             fpc = ~ NI + NII + N_i, data = muestra,
-                             nest = T)
+#*****************************************************************************
+## Estimador de Fay - Herriot ####
+#*****************************************************************************
 
-Estimadirectas <- svyby(~MATEMATICAS_PUNT, ~ID_DANE, diseno_muestral,FUN = svymean)
-Estimadirectas$vest_dir <- Estimadirectas$se^2 # INSUMO FUNDAMENTAL
-Estimadirectas$cve <- Estimadirectas$se/Estimadirectas$MATEMATICAS_PUNT * 100
+## Tenemos un prblema de 31 observaciones y 44 variables (+ 1 var respuesta)
+dim(muestra_multi)
 
-cons <- muestra %>% group_by(ID_DANE, MCPIO)  %>% 
-     summarise(temp = n())
-cons$temp <- NULL
-Estimadirectas <- merge(cons, Estimadirectas, by = "ID_DANE")
-Estimadirectas <- arrange(Estimadirectas, -MATEMATICAS_PUNT)
+## Selección de variables
+m_corr <- data.frame(meansByMun=t(cor(muestra_multi$meansByMun, muestra_multi[,11:55])))
+m_corr$feature <- row.names(m_corr)
+m_corr$meansByMun <- abs(round(m_corr$meansByMun, 1))
 
-#################################### Estimador de Fay - Herriot ####################
-Estimadirectas$MCPIO <- NULL
-# Incorporar la información auxiliar
-Datos <- merge(InfoAux,Estimadirectas, by.x = "ID_DANE", by.y = "ID_DANE", all.y=T)
-#edit(names(Datos))
+m_corr <- m_corr %>% 
+          filter(feature %in% names(Municipios)) %>%
+          arrange(desc(abs(meansByMun))) #%>% 
+          #select(feature, meansByMun) %>%
+          #group_by(meansByMun) %>%
+          #summarise(feature=last(feature))
+m_corr
+
+var_candidates <- c("CONSUMO_ENERGIA_PER_HABIT", "COBER_MEDIA_TASA", "NBI_RURAL_2010", 
+                    "DEPENDENCIA_REGALIAS_TRANSFERENCIAS",
+                    "TASA_AFILIADOS_CONTRIBUTIVO", "COBER_SECUNDARIA_TASA", "TASA_HOMICIDIOS",
+                    "Tasa_recibidos", "IND_CAPACIDAD_ADMINISTRATIVA", "IND_INTEGRAL_MCIPAL",
+                    "TASA_VACUN_BCG", "densidad_pop", "IND_EFICIENCIA_MUNICIPAL","IND_EFICACIA_MUNICIPAL",
+                    "TASA_VACUN_POLIO", "TASA_VACUN_DPT", "Tasa_secuestros") #m_corr$feature
+var_candidates
+
 # Exploración 
-modelocompleto <- paste0("MATEMATICAS_PUNT", " ~ ", paste(varcandidas, collapse = " + "))
+modelocompleto <- paste0("meansByMun", " ~ ", paste(var_candidates, collapse = " + "))
 modelocompleto <- as.formula(modelocompleto)
-varcandidas <- c("TASA_AFILIADOS_SUBSIDIADO", 
-                 "AVALUOS_CATASTRALES_RURALES", "AVALUOS_CATASTRALES_URBANOS", 
-                 "COBER_MEDIA_TASA", "COBER_PRIMARIA_TASA", "COBER_SECUNDARIA_TASA", 
-                 "COBER_TRANSICION_TASA", "CONSUMO_ENERGIA_PER_HABIT", "IND_EFICIENCIA_MUNICIPAL", 
-                 "IND_INTEGRAL_MCIPAL", "NBI_2010", 
-                 "NBI_RURAL_2010", "NBI_URBANO_2010", 
-                 "TASA_DELITOS_SEXUALES", "TASA_DESERCION_ESCOLAR", 
-                 "TASA_VIOLENCIA_INTRAFAMILIAR", "TASA_HOMICIDIOS", "TASA_VACUN_BCG", 
-                 "TASA_VACUN_DPT", "TASA_VACUN_POLIO", "TASA_VACUN_TRIPLEVIRAL")
-modelo_optimo <- step(lm(modelocompleto, data = Datos))
-modelo_reducido <- lm(MATEMATICAS_PUNT ~ TASA_AFILIADOS_SUBSIDIADO + CONSUMO_ENERGIA_PER_HABIT + 
-                           IND_INTEGRAL_MCIPAL + NBI_RURAL_2010 + NBI_URBANO_2010 + 
-                           TASA_VIOLENCIA_INTRAFAMILIAR + TASA_VACUN_DPT + TASA_VACUN_POLIO + 
-                           TASA_VACUN_TRIPLEVIRAL, data = Datos)
 
-cor(Datos[,c("MATEMATICAS_PUNT",names(modelo_reducido$coefficients)[2:10])])[1,]
-formula_candidata <- as.formula("MATEMATICAS_PUNT ~ AVALUOS_CATASTRALES_RURALES + 
-                                AVALUOS_CATASTRALES_URBANOS + CONSUMO_ENERGIA_PER_HABIT + NBI_2010")
-modelo_reducido2 <- lm(MATEMATICAS_PUNT ~ TASA_AFILIADOS_SUBSIDIADO + CONSUMO_ENERGIA_PER_HABIT + 
-                            IND_INTEGRAL_MCIPAL + NBI_RURAL_2010 + NBI_URBANO_2010 + 
-                            TASA_VACUN_DPT + TASA_VACUN_POLIO + 
-                            TASA_VACUN_TRIPLEVIRAL, data = Datos)
+modelo_optimo <- step(lm(modelocompleto, data = muestra_multi))
+modelo_optimo$call
+modelo_optimo <- names(modelo_optimo$coefficients)[2:length(names(modelo_optimo$coefficients))]
+formula_var <- paste0("meansByMun", " ~ ", paste(modelo_optimo, collapse = " + "))
 
+modelo_reducido <- lm(formula_var, data = muestra_multi)
+summary(modelo_reducido)
+var_selec <- names(which(p$coefficients[,4]<=0.05))
+formula_var <- paste0("meansByMun", " ~ ", paste(var_selec, collapse = " + "))
+
+modelo_reducido2 <- lm(formula_var, data = muestra_multi)
 summary(modelo_reducido2)
+var_sel <- names(modelo_reducido2$coefficients)[2:length(names(modelo_reducido2$coefficients))]
+
 #################################################################
-FH_prommat <- mseFH(MATEMATICAS_PUNT ~ TASA_AFILIADOS_SUBSIDIADO + CONSUMO_ENERGIA_PER_HABIT + 
-                         IND_INTEGRAL_MCIPAL + NBI_RURAL_2010 + NBI_URBANO_2010 + 
-                         TASA_VACUN_DPT + TASA_VACUN_POLIO + 
-                         TASA_VACUN_TRIPLEVIRAL, vest_dir,
-                    data = Datos)
+formula_var <- as.formula(paste0("meansByMun", " ~ ", paste(var_sel, collapse = " + ")))
+FH_prommat <- mseFH(formula_var,  varmeansByMun, data = muestra_multi)
 FH_prommat
-Datos$Y_FH <- FH_prommat$est$eblup
-Datos$mseY_FH <- FH_prommat$mse
-Datos$cve_FH <- FH_prommat$mse / FH_prommat$est$eblup * 100
-Datos$cve_dir <- sqrt(Datos$vest_dir) / Datos$MATEMATICAS_PUNT * 100
-Resultados_sd <- Datos[c("ID_DANE","MCPIO","MATEMATICAS_PUNT","cve_dir", "Y_FH", "mseY_FH","cve_FH")]
+
+datos_ingreso <- muestra_multi
+datos_ingreso$Y_FH <- FH_prommat$est$eblup
+datos_ingreso$mseY_FH <- FH_prommat$mse
+datos_ingreso$cve_FH <- FH_prommat$mse / FH_prommat$est$eblup * 100
+datos_ingreso$cve_dir <- sqrt(datos_ingreso$varmeansByMun) / datos_ingreso$meansByMun * 100
+Resultados_sd <- datos_ingreso[c("ID_MUNIC","MCPIO","meansByMun","cve_dir", "Y_FH", "mseY_FH","cve_FH")]
 names(Resultados_sd)[3] <- "Y_dir"
-mpiossel <- unique(muestra$ID_DANE)
-mpiosnosel <- InfoAux$ID_DANE[!(InfoAux$ID_DANE %in% mpiossel)]
+
+mpiossel <- unique(muestra_multi$ID_MUNIC)
+mpiosnosel <- Municipios$ID_DANE[!(Municipios$ID_DANE %in% mpiossel)]
+length(mpiossel) + length(mpiosnosel)
 
 # Hacer las estimaciones del promedio para los dominios no observados en la muestra
-X_rd <- InfoAux[InfoAux$ID_DANE %in% mpiosnosel, 
-                c("TASA_AFILIADOS_SUBSIDIADO", "CONSUMO_ENERGIA_PER_HABIT", 
-                  "IND_INTEGRAL_MCIPAL", "NBI_RURAL_2010", "NBI_URBANO_2010", 
-                  "TASA_VACUN_DPT", "TASA_VACUN_POLIO", "TASA_VACUN_TRIPLEVIRAL")]
+X_rd <- Municipios %>% filter(ID_DANE %in% mpiosnosel) %>%
+        select_(.dots = var_sel)
+head(X_rd)
 
 X_rd$Unos <- 1
-X_rd <- X_rd[c("Unos", c("TASA_AFILIADOS_SUBSIDIADO", "CONSUMO_ENERGIA_PER_HABIT", 
-                         "IND_INTEGRAL_MCIPAL", "NBI_RURAL_2010", "NBI_URBANO_2010", 
-                         "TASA_VACUN_DPT", "TASA_VACUN_POLIO", "TASA_VACUN_TRIPLEVIRAL"))]
+X_rd <- X_rd %>% select_(.dots = c("Unos", var_sel))
+
 X_rd <- as.matrix(X_rd)
-row.names(X_rd) <- InfoAux[InfoAux$ID_DANE %in% mpiosnosel,]$ID_DANE
+row.names(X_rd) <- Municipios[Municipios$ID_DANE %in% mpiosnosel,]$ID_DANE
 Beta <- as.matrix(FH_prommat$est$fit$estcoef$beta)
 YFH_rd <- X_rd %*% Beta # Estimación para los dominios no observados
 YFH_rd <- as.data.frame(YFH_rd)
-YFH_rd$ID_DANE <- InfoAux[InfoAux$ID_DANE %in% mpiosnosel, ]$ID_DANE
+YFH_rd$ID_DANE <- Municipios[Municipios$ID_DANE %in% mpiosnosel, ]$ID_DANE
 colnames(YFH_rd)[1] <- "Y_FH_rd"                             
 
 
@@ -115,18 +117,14 @@ colnames(YFH_rd)[1] <- "Y_FH_rd"
 sigma2_u <- FH_prommat$est$fit$refvar
 
 # Información auxiliar en la muestra
-X_sd <- InfoAux[InfoAux$ID_DANE %in% mpiossel, 
-                c("TASA_AFILIADOS_SUBSIDIADO", "CONSUMO_ENERGIA_PER_HABIT", 
-                  "IND_INTEGRAL_MCIPAL", "NBI_RURAL_2010", "NBI_URBANO_2010", 
-                  "TASA_VACUN_DPT", "TASA_VACUN_POLIO", "TASA_VACUN_TRIPLEVIRAL")]
+X_sd <- Municipios %>% filter(ID_DANE %in% mpiossel) %>%
+        select_(.dots = var_sel)
 X_sd$Unos <- 1
-X_sd <- X_sd[c("Unos", "TASA_AFILIADOS_SUBSIDIADO", "CONSUMO_ENERGIA_PER_HABIT", 
-               "IND_INTEGRAL_MCIPAL", "NBI_RURAL_2010", "NBI_URBANO_2010", 
-               "TASA_VACUN_DPT", "TASA_VACUN_POLIO", "TASA_VACUN_TRIPLEVIRAL")]
+X_sd <- X_sd %>% select_(.dots = c("Unos", var_sel))
 X_sd <- as.matrix(X_sd)
 
 # \hat{V}(\hat{\boldsymbol{\beta}}): varianza de los parámetros
-V_beta_est <- t(X_sd) %*% diag(sigma2_u + Datos$vest_dir) %*% X_sd  # 24 X 1
+V_beta_est <- t(X_sd) %*% diag(sigma2_u + datos_ingreso$varmeansByMun) %*% X_sd  # 24 X 1
 
 # Error cuadrático para dominios no observados
 MSE_Y.FH <- X_rd %*% solve(V_beta_est) %*% t(X_rd) + sigma2_u
@@ -136,13 +134,12 @@ colnames(MSE_Y.FH) <- c("ID_DANE", "MSE_Y_FH_rd")
 
 Resultados_rd <- merge(YFH_rd, MSE_Y.FH, by = "ID_DANE")
 # Pegarle el municipio a Resultados_rd
-Municipios <- InfoAux %>% group_by(ID_DANE, MCPIO) %>% summarise(temp = n())
-Municipios$temp <- NULL
-Resultados_rd <- merge(Resultados_rd, Municipios, by = "ID_DANE")
+nombres <- Municipios %>% group_by(ID_DANE, MCPIO) %>% summarise(temp = n()) %>% select(-(temp))
+Resultados_rd <- merge(Resultados_rd, nombres, by = "ID_DANE")
 
 # Combinarla con las estimaciones de los municipios observados
 names(Resultados_sd); names(Resultados_rd)
-names(Resultados_sd)[c(5, 6, 7)] <- c("Y_FH_sd", "MSE_Y_FH_sd", "cve_FH_sd")
+names(Resultados_sd)[c(1, 5, 6, 7)] <- c("ID_DANE","Y_FH_sd", "MSE_Y_FH_sd", "cve_FH_sd")
 
 Resultados_sd$TipologiaDominio  <- "Observado"
 Resultados_rd$TipologiaDominio  <- "No observado"
@@ -159,3 +156,7 @@ ResultadosFinales <- Resultados[c("TipologiaDominio","ID_DANE", "MCPIO", "Y_dir"
 View(ResultadosFinales)
 ResultadosFinales <- arrange(ResultadosFinales, -Y_dir)
 ResultadosFinales$Ranking <- 1:nrow(ResultadosFinales)
+
+mean(ResultadosFinales$cve_FH)
+
+
